14d934797f42470a4d03bc52b28aa84e6cd60422
Lucas Hang <lucas.hhang@gmail.com>
2021-04-20 21:54:14 -0300

    feat: created scoreCardWidget

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,6 +1,6 @@
 import 'package:devquiz/core/app_gradients.dart';
-import 'package:devquiz/core/app_images.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,40 +8,51 @@ class AppBarWidget extends PreferredSize {
     preferredSize: Size.fromHeight(250),
     child: Container(
       height: 250,
-      decoration: BoxDecoration(
-        gradient: AppGradients.linear
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Stack(
         children: [
-          Text.rich(
-            TextSpan(
-              text: "Fala, ",
-              style: AppTextStyles.title,
-              children: [
-                TextSpan(
-                  text: "Lucas!",
-                  style: AppTextStyles.titleBold,
-                )
-              ]
-            )
-          ),
           Container(
-            width: 58,
-            height: 58,
+            height: 161,
+            width: double.maxFinite,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/38769565?v=4"
-                )
-              )
+              gradient: AppGradients.linear
             ),
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: "Fala, ",
+                    style: AppTextStyles.title,
+                    children: [
+                      TextSpan(
+                        text: "Lucas!",
+                        style: AppTextStyles.titleBold,
+                      )
+                    ]
+                  )
+                ),
+                Container(
+                  width: 56,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/38769565?v=4"
+                      )
+                    )
+                  ),
+                )
+              ],
+            ), 
+          ),
+
+          Align(
+            alignment: Alignment(0.0, 1.1),
+            child: ScoreCardWidget(),
           )
-        ],
-      ),
+        ]
       )
     ),
   );
